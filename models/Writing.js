92c847662efa3b9ca9697042dfdb39ec6e1f6075
Edwin <mredwinrodriguez@gmail.com>
2020-09-17 16:32:11 -0400

    writing schema

---
+++ models/Writing.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const WritingSchema = new mongooseSchema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
+    parent_writing: {type: mongoose.Schema.Types.ObjectId, ref: 'writing'}, 
+    text: {type: string, required: true},
+    name: {type: String },
+    inspiration: {type: String },
+    date: { type: Date, default: Date.now },
+	updated: { type: Date },
+    comments: [
+		{ 
+			user: { type: Schema.Types.ObjectId, ref: 'users'},
+			text: {type: String, required: true},
+			name: {type: String },
+			date: { type: Date, default: Date.now },
+			updated: { type: Date }
+		}
+	],
+})
+
+module.exports = Writing = mongoose.model('writing', WritingSchema);
