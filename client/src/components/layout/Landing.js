bc7daf52c78a1ea9cdc35bcfa96f09e5e18109b0
Edwin <mredwinrodriguez@gmail.com>
2020-09-25 14:36:37 -0400

    navbar and landing

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Landing = () => {
+    return (
+        <section className="landing">
+        <div className="dark-overlay">
+          <div className="landing-inner">
+            <h1 className="x-large">Writers Block</h1>
+            <p className="lead">
+              Become better at writing stories, essays, books, poems and more.
+            </p>
+            <div className="buttons">
+              <a href="register.html" className="btn btn-primary">Sign Up</a>
+              <a href="login.html" className="btn btn-light">Login</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+}
+
+export default Landing;
