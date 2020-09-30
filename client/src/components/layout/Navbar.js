8ce16d3e0217aee05857f0c80f142b027f4a70e4
Edwin <mredwinrodriguez@gmail.com>
2020-09-30 13:29:00 -0400

    logout

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/authActions';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
-export const Navbar = () => {
-    return (
-        <nav className="navbar bg-dark">
-        <h1>
-          <Link to="/"><i className="fas fa-code"></i>Writers Block</Link>
-        </h1>
-        <ul>
-          <li><Link to="/register">Register</Link></li>
-          <li><Link to="/login">Login</Link></li>
-        </ul>
-      </nav>
-    )
+export const Navbar = ({ auth: {isAuthenticated, loading}, logout}) => {
+  const authLinks = (
+    <ul>
+      <a onClick={logout} href='/login'><FontAwesomeIcon icon={faSignOutAlt} /> <span className='hide-sm'>Lougout</span></a>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+      <li><Link to="/register">Register</Link></li>
+      <li><Link to="/login">Login</Link></li>
+    </ul>
+  )
+  const navbarBrand = !isAuthenticated ? (<Link to="/"><i className="fas fa-code"></i>Writers Block</Link>) : <Link to="/dashboard"><i className="fas fa-code"></i>Writers Block</Link>
+  return (
+      <nav className="navbar bg-dark">
+      <h1>
+        {navbarBrand}
+      </h1>
+      { !loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
+    </nav>
+  )
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToPropes = state => ({
+  auth: state.auth
+})
+export default connect(mapStateToPropes, {logout})(Navbar);
