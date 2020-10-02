ded88199c90702bb63a211a8305dd57f86ea96ca
Edwin <mredwinrodriguez@gmail.com>
2020-10-02 10:53:10 -0400

    redesign dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,23 +5,27 @@ import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faBookOpen } from '@fortawesome/free-solid-svg-icons';
 
 const Dashboard = ({ getCurrentProfile, auth: {user} , profile: {profile, loading}}) => {
     useEffect(() => {
         getCurrentProfile();
     }, [])
+    
 
     return loading && profile == null ? <Spinner /> : 
     <Fragment>
-       <h1 className='large text-light'>Dashboard</h1>
-       <p className='lead text-light'>
+        <h1 className='large text-primary '>Dashboard</h1>
+        <p className='lead text-dark'>
        <FontAwesomeIcon icon={faUser} /> Welcome {user && user.first_name && user.last_name ? user.first_name + " " + user.last_name: null}
        </p>
-       {profile != null ? <Fragment>Has</Fragment> : <Fragment>
-           <p className='text-light'>You have not created a profile.  Tell us more about you.</p>
-           <Link to='/create-profile' className='btn btn-primary my-1'>Create Profile</Link>
+       {profile != null ? <Fragment></Fragment> : <Fragment>
+           <div className='row'>
+            <div class="col-xs-12 col-lg-6 mb10"> You have not created a profile.  Tell us more about yourself. </div>
+            <div class="col-xs-3 col-lg-3"><Link to='/create-profile' className='btn btn-primary'>Create Profile</Link></div>
+           </div>
            </Fragment>}
+        <h3 className='large text-dark mt10'><FontAwesomeIcon icon={faBookOpen} /> My Notebooks</h3>
     </Fragment>
 }
 
